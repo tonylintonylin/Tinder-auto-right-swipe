fead52c0742861a80416f5faa7606b6edb16eb8b
Tony Lin <tonylinbusiness@gmail.com>
2018-01-01 12:43:15 -0500

    Delete like.js

--- like.js
+++
@@ -1,9 +0,0 @@
-var confirmBox = confirm('Start swiping right?');
-var count = 1;
-var like = function(){
-    if (confirmBox){
-      document.querySelector('[aria-label="Like"]').click();
-      console.log('Liked ' + count);						  count++;												};
-    setTimeout(like, 100);
-};
-setTimeout(like, 100);
